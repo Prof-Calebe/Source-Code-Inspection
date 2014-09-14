02ae519c46d2fe842196592cf4106e364eb2fe86
William <wseiki@gmail.com>
2014-09-14 16:07:16 -0300

    InterfacePrincipal - Implementado solicitarBilhete

--- Source Code Inspection/src/br/calebe/ticketmachine/core/InterfacePrincipal.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/InterfacePrincipal.java
@@ -1,25 +1,34 @@
 package br.calebe.ticketmachine.core;
 
 import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
+import br.calebe.ticketmachine.exception.SaldoInsuficienteException;
 
 public class InterfacePrincipal {
 
     public TicketMachine ticketMachine;
 
-    public InterfacePrincipal(){
+    public InterfacePrincipal() {
         int valorBilhete = 130;
         ticketMachine = new TicketMachine(valorBilhete);
     }
-    
+
     public void inserirDinheiro(int valor) {
         try {
             ticketMachine.inserir(valor);
         } catch (PapelMoedaInvalidaException e) {
             System.out.println(e.getMessage());
         }
-        
+
         int saldo = ticketMachine.getSaldo();
         System.out.println("Saldo: " + saldo);
+    }
+
+    public void solicitarBilhete() {
+        try {
+            ticketMachine.imprimir();
+        } catch (SaldoInsuficienteException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 }
