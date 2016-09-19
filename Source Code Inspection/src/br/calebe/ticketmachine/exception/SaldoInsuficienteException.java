c323c52bff39df8e3cc4781364d085cb78dc6250
Sighmir <guistoppa1995@hotmail.com>
2016-09-19 08:38:11 -0300

    Second commit
    Fixed this class to be used as an exception class.

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/SaldoInsuficienteException.java
@@ -5,4 +5,28 @@ package br.calebe.ticketmachine.exception;
  * @author Calebe de Paula Bianchini
  */
 public class SaldoInsuficienteException extends Exception {
+  public SaldoInsuficienteException ()
+    {
+        super ("Saldo insuficiente!")
+    }
+
+    public SaldoInsuficienteException ( String message )
+    {
+        super ( message );
+    }
+
+    public SaldoInsuficienteException ( String message , Throwable cause )
+    {
+        super ( message , cause );
+    }
+
+    public SaldoInsuficienteException ( Throwable cause )
+    {
+        super ( cause );
+    }
+
+    public SaldoInsuficienteException ( String message , Throwable cause , boolean enableSuppression , boolean writableStackTrace )
+    {
+        super ( message , cause , enableSuppression , writableStackTrace );
+    }
 }
