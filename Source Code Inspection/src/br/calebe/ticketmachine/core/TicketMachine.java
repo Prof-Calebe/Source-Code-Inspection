71680fe2f46ac995c7ad55d658a57e4958266171
Sighmir <guistoppa1995@hotmail.com>
2016-09-30 10:32:02 -0300

    Update TicketMachine.java
    Na linha 39 era <PapelMoeda> invez de <Integer>.

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -36,7 +36,7 @@ public class TicketMachine {
         return saldo;
     }
 
-    public Iterator<Integer> getTroco() {
+    public Iterator<PapelMoeda> getTroco() {
         Troco troco = new Troco(this.getSaldo());
         return troco.getIterator();
     }
