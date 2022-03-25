19bfbb77cdc2b1c1a6c2a730239c061032b96afe
pfreitasbarbosa <contato.pfmb@gmail.com>
2022-03-25 20:09:34 -0300

    fix: corrige a lógica para o cálculo do troco

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -12,36 +12,14 @@ class Troco {
 
     public Troco(int valor) {
         papeisMoeda = new PapelMoeda[6];
-        int count = 0;
-        while (valor % 100 != 0) {
-            count++;
+        int[] notas = {100, 50, 20, 10, 5, 2};
+        int i = 0;
+
+        for (int nota: notas) {
+            papeisMoeda[i] = new PapelMoeda(nota, (int)(valor/nota));
+            valor = valor % nota;
+            i++;
         }
-        papeisMoeda[5] = new PapelMoeda(100, count);
-        count = 0;
-        while (valor % 50 != 0) {
-            count++;
-        }
-        papeisMoeda[4] = new PapelMoeda(50, count);
-        count = 0;
-        while (valor % 20 != 0) {
-            count++;
-        }
-        papeisMoeda[3] = new PapelMoeda(20, count);
-        count = 0;
-        while (valor % 10 != 0) {
-            count++;
-        }
-        papeisMoeda[2] = new PapelMoeda(10, count);
-        count = 0;
-        while (valor % 5 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(5, count);
-        count = 0;
-        while (valor % 2 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(2, count);
     }
 
     public Iterator<PapelMoeda> getIterator() {
