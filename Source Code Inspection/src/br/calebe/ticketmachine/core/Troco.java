8e8077b6d56655f8007a881b2914a09b9536ecc0
JOAO VICTOR SERRALHA M LIMA <unifjlima@feinet.fei.edu.br>
2022-09-23 22:15:35 -0300

    fix: Adding correct looping condition

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -14,32 +14,32 @@ class Troco {
     public Troco(int valor) {
         papeisMoeda = new PapelMoeda[6];
         int count = 0;
-        while (valor % 100 != 0) {
+        while (valor % 100 == 0) {
             count++;
         }
         papeisMoeda[5] = new PapelMoeda(100, count);
         count = 0;
-        while (valor % 50 != 0) {
+        while (valor % 50 == 0) {
             count++;
         }
         papeisMoeda[4] = new PapelMoeda(50, count);
         count = 0;
-        while (valor % 20 != 0) {
+        while (valor % 20 == 0) {
             count++;
         }
         papeisMoeda[3] = new PapelMoeda(20, count);
         count = 0;
-        while (valor % 10 != 0) {
+        while (valor % 10 == 0) {
             count++;
         }
         papeisMoeda[2] = new PapelMoeda(10, count);
         count = 0;
-        while (valor % 5 != 0) {
+        while (valor % 5 == 0) {
             count++;
         }
         papeisMoeda[1] = new PapelMoeda(5, count);
         count = 0;
-        while (valor % 2 != 0) {
+        while (valor % 2 == 0) {
             count++;
         }
         papeisMoeda[0] = new PapelMoeda(2, count); 
