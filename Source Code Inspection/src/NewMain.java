60c29be3046499f31e83c06012ce5159e0ca2145
EijiDogen <eijikd@hotmail.com>
2023-03-30 20:11:11 -0300

    Added a Main Class

---
+++ Source Code Inspection/src/NewMain.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+import br.calebe.ticketmachine.core.TicketMachine;
+import br.calebe.ticketmachine.exception.PapelMoedaInvalidaException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author eijik
+ */
+
+
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
