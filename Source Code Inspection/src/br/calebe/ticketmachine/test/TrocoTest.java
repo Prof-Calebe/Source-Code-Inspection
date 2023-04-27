59fbcec8b738c89dde8dd8d72dd38b25258bd895
Shawn Kawabe <59843897+shawnkawabe@users.noreply.github.com>
2023-04-27 19:11:30 -0300

    update tests pipeline

---
+++ Source Code Inspection/src/br/calebe/ticketmachine/test/TrocoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+  @Test
+  public void evaluatesExpression() {
+    Troco troco = new Troco();
+    assertEquals(1, 1);
+  }
+}
