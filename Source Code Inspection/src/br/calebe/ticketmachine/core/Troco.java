49b8250c5511bdd6460c5e555e77109a45cc7727
Enzo Tatiyama <enzo.tatiyama@gmail.com>
2023-09-28 22:04:40 -0300

    fix: infinite loops on Troco constructor

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -9,39 +9,19 @@ import java.util.Iterator;
 class Troco {
 
     protected PapelMoeda[] papeisMoeda;
+    protected int[] papelMoeda = {100, 50, 20, 10, 5, 2};
 
     public Troco(int valor) {
         papeisMoeda = new PapelMoeda[6];
-        int count = 0;
-        while (valor % 100 != 0) {
-            count++;
+
+        for (int i = 5; i >= 0; i--) {
+            int count = 0;
+            while (valor % papelMoeda[i] != 0) {
+                count++;
+                valor -= papelMoeda[i];
+            }
+            papeisMoeda[i] = new PapelMoeda(papelMoeda[i], count);
         }
-        papeisMoeda[5] = new PapelMoeda(100, count);
-        count = 0;
-        while (valor % 50 != 0) {
-            count++;
-        }
-        papeisMoeda[4] = new PapelMoeda(50, count);
-        count = 0;
-        while (valor % 20 != 0) {
-            count++;
-        }
-        papeisMoeda[3] = new PapelMoeda(20, count);
-        count = 0;
-        while (valor % 10 != 0) {
-            count++;
-        }
-        papeisMoeda[2] = new PapelMoeda(10, count);
-        count = 0;
-        while (valor % 5 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(5, count);
-        count = 0;
-        while (valor % 2 != 0) {
-            count++;
-        }
-        papeisMoeda[1] = new PapelMoeda(2, count);
     }
 
     public Iterator<PapelMoeda> getIterator() {
