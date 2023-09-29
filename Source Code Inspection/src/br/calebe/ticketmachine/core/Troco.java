227e4e313515cddeff41a5989cf4b7d8eb35f21c
enzopel <73806739+EnzoPellegrini@users.noreply.github.com>
2023-09-29 16:25:55 +0000

    feat: correção issue #9 - O valor esta sendo atribuido a posição incorreta do array papelMoeda na classe Troco

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -47,7 +47,7 @@ class Troco {
             valor -= 2;
             count++;
         }
-        papeisMoeda[1] = new PapelMoeda(2, count);
+        papeisMoeda[0] = new PapelMoeda(2, count);
     }
 
     public Iterator<PapelMoeda> getIterator() {
